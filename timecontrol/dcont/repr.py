85f29f6148d6cd53dd7c17aba3e6ee7ac2f8537c
AlexV <asmodehn@gmail.com>
2020-01-18 21:25:54 +0100

    playing around with representation of datatypes

---
+++ timecontrol/dcont/repr.py
@@ -0,0 +1,62 @@
+"""
+
+Implementing an isomorphic repr (useful for our containers, especially dict/tree)
+
+"""
+from functools import singledispatch
+
+import typing
+
+from typeclasses import typeclass
+
+T = typing.TypeVar('T')
+
+
+@typeclass(T)
+def isorepr(t: T):
+    raise NotImplementedError
+
+
+@isorepr.instance(str)
+def _(t: str):
+    return f"{t}: str"  # a string is its own representation
+
+
+@isorepr.instance(int)
+def _(i: int):
+    return f"{i}: int"
+
+
+def parserepr(r: str) -> typing.Any:
+    """
+    The parser inverse of the various repr.
+    """
+
+    # TODO : find a cleaner implementation...
+
+    t = r[-3:]
+
+    p = globals().get(t)
+    if p is None:
+        import builtins
+        p = vars(builtins).get(t)
+    return p(r[:-3-2])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    r34 = isorepr(34)
+    pr34 = parserepr(r34)
+    assert pr34 == 34, pr34
+
+    r42 = isorepr('42')
+    pr42 = parserepr(r42)
+    assert pr42 == '42', pr42
+
+
+
