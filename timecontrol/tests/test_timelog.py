9dca56fba69b721a1582369fb8b348e461c7b2b8
AlexV <asmodehn@gmail.com>
2020-03-08 11:40:51 +0100

    a different (sync) way to do time-based logging...

--- timecontrol/tests/test_timelog.py
+++ timecontrol/tests/test_timelog.py
@@ -30,7 +30,7 @@ class TestTimeLog(aiounittest.AsyncTestCase):
         tl = timelog(mapping={t: i for t, i in zip(til, value_list)})
         # test that if contained, we can get it, and dual
 
-        dt = data.draw(st.datetimes())
+        dt = data.draw(st.integers(min_value=tl.begin, max_value=tl.end))
 
         if dt in tl:
             # make sure we have a directed container = > we get the same type of container
@@ -54,10 +54,85 @@ class TestTimeLog(aiounittest.AsyncTestCase):
         # actually equal
         assert tlb == tl == tlb  # verifying commutativity of equality
 
+    def test_call_comonad(self):
+        raise NotImplementedError
 
-    # async def test_aiter(self):
-    #     # test comonadic that we can iterate on it to the future (and block when needed)
-    #     raise NotImplementedError
+    # we do not depend on hypothesis here, as this can be quite complex and take some time
+    # def test_aiter_comonad(self):
+    #     import asyncio
+    #     import random
+    #
+    #     # a drivable clock for testing
+    #     test_clock = 0
+    #
+    #     def clock():
+    #         return test_clock
+    #
+    #     tl = timelog(mapping=dict(), timer_ns = clock)
+    #
+    #     # test comonadic that we can iterate on it towards the future
+    #     # extract M a -> a
+    #
+    #     inserted = []
+    #     extracted = []
+    #
+    #     async def putin(drives: bool = False):
+    #         nonlocal test_clock, inserted
+    #         tf = tl()  # generator
+    #         while tf:
+    #             nextv = random.randint(1, 42)
+    #             inserted.append(nextv)
+    #             tf.send(nextv)  # sink
+    #             if drives:  # one tick in one loop, to prevent compressing values
+    #                 test_clock += tl.timeframe_ns
+    #
+    #     async def getout(drives: bool = False):
+    #         nonlocal test_clock, extracted
+    #         async for v in tl:  # source
+    #             assert isinstance(v, int)
+    #             extracted.append(v)
+    #             if drives:  # one tick in one loop, to prevent accumulating values
+    #                 test_clock += tl.timeframe_ns
+    #
+    #     async def sched_pull(loop):
+    #         put = loop.create_task(putin())
+    #         get = loop.create_task(getout(True))
+    #
+    #         await asyncio.sleep(10)
+    #         put.cancel()
+    #         get.cancel()
+    #
+    #     async def sched_push(loop):
+    #         put = loop.create_task(putin(True))
+    #         get = loop.create_task(getout())
+    #
+    #         await asyncio.sleep(10)
+    #         get.cancel()
+    #         put.cancel()
+    #
+    #     loop = asyncio.get_event_loop()
+    #     asyncio.run(sched_pull(loop))
+    #
+    #     assert inserted
+    #     assert extracted
+    #     assert inserted == extracted, f"{inserted} => {extracted}"
+    #
+    #     # reset
+    #     inserted.clear()
+    #     extracted.clear()
+    #
+    #     asyncio.run(sched_push(loop))
+    #
+    #     assert inserted
+    #     assert extracted
+    #     assert inserted == extracted, f"{inserted} => {extracted}"
+    #
+    #     # duplicate M a -> M M a
+    #     # since we are a bimonad we can be even stronger here
+    #     tlb = timelog(tl)
+    #
+    #     # actually equal
+    #     assert tlb == tl == tlb  # verifying commutativity of equality
 
 
 if __name__ == "__main__":
--- timecontrol/timelog.py
+++ timecontrol/timelog.py
@@ -3,25 +3,29 @@ from __future__ import annotations
 import itertools
 import time
 from asyncio import Event
-from collections import Mapping, MutableMapping, OrderedDict
-
+from collections import OrderedDict
+from collections.abc import Mapping
 import typing
 
 # TODO : generic span-key based mapping
 
 # For now : timeinterval based dict
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
-from timecontrol.timeinterval import TimeInterval, timeinterval
+from timecontrol.timeinterval import TimeInterval, timeinterval, timep
 
+
+# TODO : timezone wrapper to depend onlocation => datetimelog
 
 class TimeLog(Mapping):
     """
     This class implement a timeinterval based immutable mapping store (dict)
     We do not care here about values, that can be anything.
+
+    Note: the keys could be sparsely distributed along the time axis...
     """
-    def __init__(self, mapping: typing.Mapping,
-                 timer_ns: typing.Callable[[], int] = time.monotonic_ns, timeframe_ns: int = 1):
+    def __init__(self, mapping: typing.MutableMapping,
+                 timer_ns: typing.Callable[[], int] = time.time_ns, timeframe_ns: int = 1):
         # INTERNAL Time measurement (for in/out sync only)
         self.timer_ns = timer_ns
         self.timeframe_ns = timeframe_ns
@@ -29,23 +33,49 @@ class TimeLog(Mapping):
         self._l = mapping
         self._wait = OrderedDict()  #  a dict of events, by timedate (timelog as well ??)
 
+    @property
+    def now_ns(self) -> int:
+        return self.timer_ns()
+
+    @property
+    def begin(self) -> int:
+        if self._l:
+            return min(k.start for k in self._l.keys())
+        else:
+            return self.now_ns
+
+    @property
+    def end(self):
+        if self._l:
+            return max(k.stop for k in self._l.keys())
+        else:
+            return None  # no end yet... #TODO : proper semantics ?
+
+    @property
+    def timeindex(self):
+        return (k for k in self._l.keys())
+
+    @property
+    def log(self):
+        return (v for v in self._l.values())
+
     def __eq__(self, other):
-        return all(k == kk and s == o for (k, s), (kk, o) in itertools.zip_longest(self, other))
+        return all(k == kk and s == o for (k, s), (kk, o) in itertools.zip_longest(self, other))  # TODO : or just zip ?
 
     def __repr__(self):
-        # howtodo repr ? some kind of ohlcv ??
-        return f"{min(self._l.keys()).start}..{max(self._l.keys()).stop} : {len(self._l.values())}"
+        # howtodo repr ? some kind of ohlc ??
+        return f"{self.begin}..{self.end} : {len(self._l.values())}"
 
-    def __contains__(self, item: typing.Union[datetime, TimeInterval]):
-        if isinstance(item, datetime):
+    def __contains__(self, item: typing.Union[timep, TimeInterval]):
+        if isinstance(item, timep):
             item = timeinterval(item)
 
         for k, v in self._l.items():
             if item in k:
                 return True
 
-    def __getitem__(self, item: typing.Union[datetime, TimeInterval]):
-        if isinstance(item, datetime):
+    def __getitem__(self, item: typing.Union[timep, TimeInterval]):
+        if isinstance(item, timep):
             item = timeinterval(item)
 
         # get item extract submapping of containing timespan
@@ -56,6 +86,16 @@ class TimeLog(Mapping):
 
         return timelog(res)  # pass the mapping directly
 
+    # Immutable mapping except by calling, which is picky about args...
+    # def __setitem__(self, key, value):
+    #     if max(key, *self.timeindex) == key:
+    #         self._l.setdefault(key, value)
+    #         self._l[key] = value  # does the appropriate thing ...  # TODO : use typeddict
+    #
+    # def __delitem__(self, key):
+    #     if min(key, self._l.keys()) == key:
+    #         del self._l[key]
+
     def __len__(self):
         return len(self._l)
 
@@ -63,48 +103,139 @@ class TimeLog(Mapping):
         for k, v in reversed(self._l.items()):
             yield k, v
 
-    async def __aiter__(self):  # extract future ( blocking )
+    def __call__(self):
+        now = self.timer_ns()
+        ti = TimeInterval(start=now, stop=now + self.timeframe_ns)
+        # if we already have a larger timeframe : use it
+        for k in self._l.keys():
+            if now in k:
+                ti = k
+                break
 
-        now = datetime.fromtimestamp(self.timer_ns(), tz=timezone.utc)
+        self._l[ti] = 0  # we need to initialize the accumulator/CRDT...
 
-        async for k, v in self[now]:
-            yield k, v
-        # then wait for next event
-        wait_for_content = Event()
-        while True:
-            now = datetime.fromtimestamp(self.timer_ns(), tz = timezone.utc)
+        while self.timer_ns() in ti:
+            new_v = yield ti, self._l[ti]
+            if new_v is not None:
+                self._l[ti] = self._l[ti] + new_v  # disordered accumulation semantics...
 
-            if not now in self:
-                # before using we should clear the event in any case
-                wait_for_content.clear()
-                self._wait[now] = wait_for_content
-                await wait_for_content.wait()  # block until we get content
+        return
 
-            # we got content, yield it !
-            async for k, v in self[now]:
-                yield k, v
+    # async def __aiter__(self):  # extract future ( blocking )
+    #     # CAREFUL : here we need the local timer to be related to the wallclock
+    #     # TODO : monotonicity ??
+    #     now = self.timer_ns()
+    #
+    #     # return current values for "now" if there is any (could be if stored and called in same timeframe)
+    #     for k in self._l.keys():
+    #         if now in k:
+    #             yield k, self._l[k]
+    #
+    #     # then wait for next event
+    #     wait_for_content = Event()
+    #     while True:
+    #         now = self.timer_ns()
+    #
+    #         if not now in self:
+    #             # before using we should clear the event in any case
+    #             wait_for_content.clear()
+    #             self._wait[now] = wait_for_content
+    #             await wait_for_content.wait()  # block until we get content
+    #
+    #         # we got content, yield it !
+    #         for k in self._l.keys():
+    #             if now in k:
+    #                 yield k, self._l[k]
+    #
+    #         if now in self._wait:
+    #             # remove event if needed
+    #             self._wait.pop(now)
 
-            if now in self._wait:
-                # remove event if needed
-                self._wait.pop(now)
 
-
-EmptyTimeLog = TimeLog(mapping=OrderedDict())
-
-
-# Note : timelog is immutable
-def timelog(mapping: typing.Mapping):
-    if not mapping:
-        return EmptyTimeLog
-
+def timelog(mapping: typing.Mapping, timer_ns = time.time_ns, timeframe_ns = 1):
     if isinstance(mapping, TimeLog):
         # we have ot use the reverse of the items iterator to keep the order !
         od = OrderedDict(reversed([e for e in mapping]))  # TODO : or access internal mapping ?
     else:
         # to be able to grab data from any mapping, we use the items() interface
         od = OrderedDict(mapping.items())
-    return TimeLog(od)
+    return TimeLog(od, timer_ns=timer_ns, timeframe_ns=timeframe_ns)
 
 
 if __name__ == '__main__':
-    pass
+    import hypothesis.strategies as st
+    from timecontrol.tests.strats.st_timeinterval import st_timeinterval
+
+    # building intervals for testing
+    dt1 = time.time_ns()
+    time.sleep(1)
+    dt2 =time.time_ns()
+    time.sleep(1)
+    dt3 = time.time_ns()
+    time.sleep(1)
+    dt4 = time.time_ns()
+
+    til = [
+        TimeInterval(dt1, dt2),
+        TimeInterval(dt3, dt4)
+    ]
+    # values
+    val = [2, 1]
+
+    # example timelog
+    tl = timelog(mapping={t: i for t, i in zip(til, val)}, timeframe_ns=1000_000_000)
+
+    for t, e in tl:
+        print(f"{t} : {e}")
+
+    extracted = []
+
+    acc = tl()
+
+    curti, initv = acc.send(None)
+    try:
+        print()
+        print(acc.send(42))
+        time.sleep(3)
+        print(acc.send(42))
+    except StopIteration as si:
+        print(f"{curti} ended already.")
+
+    # print(acc.send(42))
+    # for i, st in tl():
+    #     print(f"=>> {st}")
+
+    #
+    # import asyncio
+    # import random
+    # loop = asyncio.get_event_loop()
+    # t = loop.create_task(wait_loop_fwd())
+    #
+    # # We HAVE TO create the next interval after waiting for it
+    # # otherwise we ll wait for ever since it has already happen, there will be nothing "new"
+    #
+    # # next value
+    # nextt = timeinterval(time.time_ns())
+    # nextv = 999  # special int value
+    #
+    # try :
+    #     async def insert_n_loop():
+    #         while nextv not in extracted:
+    #             time.sleep(0.5)
+    #             if not random.randint(0, 5):
+    #                 print("inserting !")
+    #                 tl(nextt, nextv)  # insert and wait for extraction
+    #             else:
+    #                 print("waiting...")
+    #
+    #     loop.run_until_complete(insert_n_loop())  # should exit when inserted value has ben extracted
+    #
+    #     # Here we are sure that we extracted what we wanted
+    #     assert nextv in extracted
+    #
+    #     # cancel task.
+    #     t.cancel()
+    # except KeyboardInterrupt:
+    #     t.result()
+    # finally:
+    #     loop.close()
