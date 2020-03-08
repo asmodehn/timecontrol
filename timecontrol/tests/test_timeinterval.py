7ba91f6822144cbb5e6ccff80e9a065a726614c2
AlexV <asmodehn@gmail.com>
2020-03-08 10:09:55 +0100

    fixing timeinterval meet to match countinterval

--- timecontrol/tests/test_timeinterval.py
+++ timecontrol/tests/test_timeinterval.py
@@ -4,40 +4,95 @@ from datetime import datetime, timedelta, MINYEAR, MAXYEAR
 import typing
 from hypothesis import given
 from hypothesis import infer
-from hypothesis.strategies import data, timedeltas, datetimes
+from hypothesis.strategies import data, timedeltas, datetimes, integers
 
-from timecontrol.timeinterval import TimeInterval
+from timecontrol.tests.strats.st_timeinterval import st_timep
+from timecontrol.timeinterval import TimeInterval, timep
 
 
 class TestTimeInterval(unittest.TestCase):
 
-    def assert_all(self, t1: typing.Union[datetime,TimeInterval], t2: typing.Union[datetime, TimeInterval],
+    def assert_all(self, t1: typing.Union[timep, TimeInterval], t2: typing.Union[timep, TimeInterval],
                    after: bool = False, before: bool = False, meets: bool = False,
                    overlaps: bool = False, during: bool = False, starts: bool = False, equal: bool = False,
                    finishes: bool = False):
+
         # before/after
-        assert after == t1.after(t2) and after == t2.before(t1), f"{t1} after {t2} and {t2} before {t1}"
-        assert before == t1.before(t2) and before == t2.after(t1), f"{t1} before {t2} and {t2} after {t1}"
+        if isinstance(t1, TimeInterval):
+            assert after == t1.after(t2), f"{t1} after {t2}"
+            assert before == t1.before(t2), f"{t1} before {t2}"
+        else:
+            assert after == (t1 > t2), f"{t1} > {t2}"
+            assert before == (t1 < t2), f"{t1} < {t2}"
+        if isinstance(t2, TimeInterval):
+            assert after == t2.before(t1), f"{t2} before {t1}"
+            assert before == t2.after(t1), f"{t2} after {t1}"
+        else:
+            assert after == (t2 < t1), f"{t2} < {t1}"
+            assert before == (t2 > t1), f"{t2} > {t1}"
+
         # meets
-        assert meets == t1.meets(t2) and meets == t2.meets_inv(t1),f"{t1} meets {t2} and {t2} meets_inv {t1}"
+        if isinstance(t1, TimeInterval):
+            assert meets == t1.meets(t2), f"{t1} meets {t2}"
+        else:
+            assert meets == (t1 == t2.start), f"{t1} == {t2.start} bound of {t2}"
+        if isinstance(t2, TimeInterval):
+            assert meets == t2.meets_inv(t1), f"{t2} meets_inv {t1}"
+        else:
+            assert meets == (t2 == t1.stop), f"{t2} == {t1.stop} bound of {t1}"
+
         # overlaps
-        assert overlaps == t1.overlaps(t2) and overlaps == t2.overlaps_inv(t1), f"{t1} overlaps {t2} and {t2} overlaps_inv {t1}"
+        if isinstance(t1, TimeInterval):
+            assert overlaps == t1.overlaps(t2), f"{t1} overlaps {t2}"
+        if isinstance(t2, TimeInterval):
+            assert overlaps == t2.overlaps_inv(t1), f"{t2} overlaps_inv {t1}"
+        if overlaps and (isinstance(t1, int) or isinstance(t2, int)):
+            raise RuntimeError(" This is not applicable to ints !")
+
         # during
-        assert during == t1.during(t2) and during == t2.during_inv(t1), f"{t1} during {t2} and {t2} during_inv {t1}"
+        if isinstance(t1, TimeInterval):
+            assert during == t1.during(t2), f"{t1} during {t2}"
+        else:
+            assert during == (t1 in t2), f"{t1} in {t2}"  # fallback on contains
+
+        if isinstance(t2, TimeInterval):
+            assert during == t2.during_inv(t1), f"{t2} during_inv {t1}"
+        else:
+            assert during == t1.during(t2), f"{t1} during {t2}"  # fallback on inverse relation (symmetric)
+
         # starts
-        assert starts == t1.starts(t2) and starts == t2.starts_inv(t1), f"{t1} starts {t2} and {t2} start_inv {t1}"
+        if isinstance(t1, TimeInterval):
+            assert starts == t1.starts(t2), f"{t1} starts {t2}"
+        else:
+            assert starts == (t1 == t2.start), f"{t1} == {t2.start}"
+
+        if isinstance(t2, TimeInterval):
+            assert starts == t2.starts_inv(t1), f"{t2} start_inv {t1}"
+        else:
+            assert starts == (t1 == t2)  # fallback on equality TODO : WHAT ELSE ???
+
         # finishes
-        assert finishes == t1.finishes(t2) and finishes == t2.finishes_inv(t1), f"{t1} finishes {t2} and {t2} finishes_inv {t1}"
+        if isinstance(t1, TimeInterval):
+            assert finishes == t1.finishes(t2), f"{t1} finishes {t2}"
+        else:
+            assert finishes == (t1 == t2.stop)
+
+        if isinstance(t2, TimeInterval):
+            assert finishes == t2.finishes_inv(t1), f"{t2} finishes_inv {t1}"
+        else:
+            assert finishes == (t1 == t2)  # fallback on equality TODO : WHAT ELSE ???
+
         # equal
+        # same for interval or ints
         assert equal == (t1 == t2) and equal == (t2 == t1), f"{t1} equals {t2} and {t2} equals {t1}"
 
     @given(dt1=infer, data=data())
-    def test_interval_before(self, dt1:datetime, data):
+    def test_interval_before(self, dt1:timep, data):
 
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
-        dt4 = data.draw(datetimes(min_value=dt3 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 +1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
+        dt4 = data.draw(st_timep(min_value=dt3 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt2)
         ti2 = TimeInterval(start=dt3, stop=dt4)
@@ -46,11 +101,11 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, before=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_before_datetime(self, dt1:datetime, data):
+    def test_interval_before_timep(self, dt1:timep, data):
 
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt2)
 
@@ -58,12 +113,12 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, dt3, before=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_after(self, dt1:datetime, data):
+    def test_interval_after(self, dt1:timep, data):
 
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
-        dt4 = data.draw(datetimes(min_value=dt3 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
+        dt4 = data.draw(st_timep(min_value=dt3 + 1))
 
         ti1 = TimeInterval(start=dt3, stop=dt4)
         ti2 = TimeInterval(start=dt1, stop=dt2)
@@ -72,12 +127,12 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, after=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_meets(self, dt1: datetime, data):
+    def test_interval_meets(self, dt1: timep, data):
 
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
         dt3 = dt2
-        dt4 = data.draw(datetimes(min_value=dt3 + timedelta(microseconds=1)))
+        dt4 = data.draw(st_timep(min_value=dt3 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt2)
         ti2 = TimeInterval(start=dt3, stop=dt4)
@@ -85,11 +140,11 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, meets=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_overlaps(self, dt1: datetime, data):
+    def test_interval_overlaps(self, dt1: timep, data):
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
-        dt4 = data.draw(datetimes(min_value=dt3 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
+        dt4 = data.draw(st_timep(min_value=dt3 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt3)
         ti2 = TimeInterval(start=dt2, stop=dt4)
@@ -97,11 +152,11 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, overlaps=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_during(self, dt1: datetime, data):
+    def test_interval_during(self, dt1: timep, data):
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
-        dt4 = data.draw(datetimes(min_value=dt3 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
+        dt4 = data.draw(st_timep(min_value=dt3 + 1))
 
         ti1 = TimeInterval(start=dt2, stop=dt3)
         ti2 = TimeInterval(start=dt1, stop=dt4)
@@ -110,10 +165,10 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, during=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_starts(self, dt1: datetime, data):
+    def test_interval_starts(self, dt1: timep, data):
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt2)
         ti2 = TimeInterval(start=dt1, stop=dt3)
@@ -121,10 +176,10 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, starts=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_finishes(self, dt1: datetime, data):
+    def test_interval_finishes(self, dt1: timep, data):
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
-        dt3 = data.draw(datetimes(min_value=dt2 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
+        dt3 = data.draw(st_timep(min_value=dt2 + 1))
 
         ti1 = TimeInterval(start=dt2, stop=dt3)
         ti2 = TimeInterval(start=dt1, stop=dt3)
@@ -132,9 +187,9 @@ class TestTimeInterval(unittest.TestCase):
         self.assert_all(ti1, ti2, finishes=True)
 
     @given(dt1=infer, data=data())
-    def test_interval_equals(self, dt1: datetime, data):
+    def test_interval_equals(self, dt1: timep, data):
         # generating other datetimes, preventing equality
-        dt2 = data.draw(datetimes(min_value=dt1 + timedelta(microseconds=1)))
+        dt2 = data.draw(st_timep(min_value=dt1 + 1))
 
         ti1 = TimeInterval(start=dt1, stop=dt2)
         ti2 = TimeInterval(start=dt1, stop=dt2)
--- timecontrol/timeinterval.py
+++ timecontrol/timeinterval.py
@@ -1,44 +1,45 @@
 from __future__ import annotations
+
+import time
 from datetime import datetime, timedelta, MAXYEAR, MINYEAR
 from enum import Enum
 
 import typing
 
-from collections import namedtuple
-from typing import TypeVar
 
-# For now : specific time span:
+# defining a time-point type ( restriction : > epoch, etc.)
+timep = int
 
-week = timedelta(weeks=1)
-day = timedelta(days=1)
-hour = timedelta(hours=1)
-minute = timedelta(minutes=1)
-second = timedelta(seconds=1)
-millisecond = timedelta(milliseconds=1)
-microsecond = timedelta(microseconds=1)
+# defining a time-delta type (diff restriction and properties
+timed = int
 
 
 class TimeInterval:
     """
-    A time interval is a couple of timedate (timedelta is deduced).
+    A time interval is a couple of timep.
 
     We rely on this representation to base everything on interval (for simple yet safe real-world correspondance)
     a time delta is considered an idea, an abstraction. we want to remain imperative / practical
     and keep the code directly related to the real world, python style, for easy maintenance.
     """
 
-    start: datetime
-    stop: datetime
+    # Note : critical semantic difference between countinterval and timeinterval.
+    # For count, since it can only increase, the start is the locally certain count.
+    #  and stop is the uncertain possible max count. it will converge somewhere inside itself.
+    # For time, since it can only increase, but we can only rely our local clock, start is the past timestamp declared,
+    #  and stop is the locally measured (certain max) of the event.
+    start: timep
+    stop: timep
 
     @property
-    def delta(self) -> timedelta:
+    def delta(self) -> timed:
         return self.stop - self.start
 
     @property
-    def median(self) -> datetime:
-        return self.start + (self.stop - self.start) / 2
+    def median(self) -> timep:
+        return self.start + (self.stop - self.start) // 2
 
-    def __init__(self, start: datetime = None, stop: datetime = None):
+    def __init__(self, start: timep = None, stop: timep = None):
         if start > stop:  # invert if needed to keep usual interval semantics
             self.start = stop
         else:
@@ -53,16 +54,22 @@ class TimeInterval:
         return f"{str(self.start)}..{str(self.stop)}"
 
     # Allen's Interval Algebra: https://en.wikipedia.org/wiki/Allen%27s_interval_algebra
-    def __eq__(self, other: TimeInterval):
+    def __eq__(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self.start == self.stop == other
         return self.start == other.start and self.stop == other.stop
 
     def __hash__(self):
         return hash((self.start, self.stop))
 
-    def after(self, other: TimeInterval):
+    def after(self, other: typing.Union[timep,TimeInterval]):
+        if isinstance(other, timep):
+            return self.start > other
         return self.start > other.stop
 
-    def before(self, other: TimeInterval):
+    def before(self, other: typing.Union[timep,TimeInterval]):
+        if isinstance(other, timep):
+            return self.stop < other
         return self.stop < other.start
 
     def __lt__(self, other: TimeInterval):
@@ -71,44 +78,66 @@ class TimeInterval:
     def __gt__(self, other: TimeInterval):
         return self.after(other)
 
-    def meets(self, other: TimeInterval):
+    def meets(self, other: typing.Union[timep,TimeInterval]):
+        if isinstance(other, timep):
+            return other == self.stop
         return self.stop == other.start
 
-    def meets_inv(self, other: TimeInterval):
+    def meets_inv(self, other: typing.Union[timep,TimeInterval]):
+        if isinstance(other, timep):
+            return self.start == other
         return self.start == other.stop
 
-    def overlaps(self, other: TimeInterval):
+    def overlaps(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            # ?? False for now (simplest boolean semantics)
+            return  False # TODO : what ? fallback on another relation ? which one ?
         return self.start < other.start < self.stop < other.stop
 
-    def overlaps_inv(self, other: TimeInterval):
+    def overlaps_inv(self, other:  typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            # ?? False for now (simplest boolean semantics)
+            return False  # TODO : what ? fallback on another relation ? which one ?
         return other.start < self.start < other.stop < self.stop
 
-    def starts(self, other: TimeInterval):
+    def starts(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self == other  # fallback on equality
         return self.start == other.start and self.stop < other.stop
 
-    def starts_inv(self, other: TimeInterval):
+    def starts_inv(self, other:  typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self.start == other  # lower bound equality sufficient
         return self.start == other.start and self.stop > other.stop
 
-    def during(self, other: TimeInterval):
+    def during(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return False  # not possible ?? TODO : what about equality ?
         return other.start < self.start and self.stop < other.stop
 
-    def during_inv(self, other: TimeInterval):
+    def during_inv(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self.start < other < self.stop
         return self.start < other.start and other.stop < self.stop
 
-    def __contains__(self, item: TimeInterval):
+    def __contains__(self, item: typing.Union[timep, TimeInterval]):
         return self.during_inv(item)
 
-    def finishes(self, other: TimeInterval):
+    def finishes(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self == other # fallback on equality
         return self.stop == other.stop and self.start > other.start
 
-    def finishes_inv(self, other: TimeInterval):
+    def finishes_inv(self, other: typing.Union[timep, TimeInterval]):
+        if isinstance(other, timep):
+            return self.stop == other  # higher bound equality sufficient
         return self.stop == other.stop and self.start < other.start
 
 
 # TODO : conversion to int slices for time.time() based timing
 
 # TODO : there is an equivalent representation with start and duration => how to support it here ?
-def timeinterval(start: datetime = None, stop: datetime=None) -> TimeInterval:
+def timeinterval(start: timep = None, stop: timep=None) -> TimeInterval:
     """
     A time slice with a [now..future] bias on creation
     :param start: start of the interval (lower bound)
@@ -116,27 +145,25 @@ def timeinterval(start: datetime = None, stop: datetime=None) -> TimeInterval:
     :return:
     """
     # sensible defaults
+    # Not the timep type is appropriate to describe time interval from now to infinite.
+    # TimeInterval needs to be BOUND on both sides, somehow.
     if start is None:
-        start = datetime.now()
+        start = time.time_ns()
     if stop is None:
-        stop = start + timedelta(microseconds=1)  # specific semantics of datetime as interval, looking ahead
-        # stop = datetime(year=MAXYEAR, month=12, day=31)
-        # stop = datetime(year=MINYEAR, month=1, day=1)
+        stop = time.time_ns()
 
     return TimeInterval(start=start, stop=stop)
 
 
 if __name__ == '__main__':
 
-    delta = week * 3
+    now = time.time_ns()
 
-    now = datetime.now()
-
-    t1 = timeinterval(now, stop=now + timedelta(days= 3))
-    t2 = timeinterval(now, stop=now + timedelta(days= 2))
+    t1 = timeinterval(now, stop=now + 1000)
+    t2 = timeinterval(now, stop=now + 2000)
 
     assert not t2 in t1
-    assert t2.starts(t1)
+    assert t1.starts(t2)
     assert not t1.finishes(t2)
 
 
