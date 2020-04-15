e319997a44c2a51f37f14cc681c829def27844da
AlexV <asmodehn@gmail.com>
2020-04-15 11:50:06 +0200

    adding product via decorator

--- timecontrol/ant.py
+++ timecontrol/ant.py
@@ -6,19 +6,42 @@ from collections import OrderedDict
 from typing import NamedTuple, Type
 
 import typing
+
+import wrapt
 from _pytest.config import ExitCode
 from hypothesis import given, infer
 import hypothesis.strategies as st
 
 
-def algebraic(pydef):
-    pass
+def algebraic(pydef: typing.Union[NamedTuple, typing.Callable[..., NamedTuple]]):
+
+    def product(self, other: typing.Union[NamedTuple, typing.Dict[str, Type], typing.Iterable[typing.Tuple[str, Type]]]):
+        return NamedTuple(typename=f"{type(self).__name__}_x_{type(other).__name__}",
+                            fields={**self.nt._field_types, **other._field_types})
+
+    @wrapt.decorator
+    def factory_decorator(wrapped, instance, args, kwargs):
+        nt = wrapped(*args, **kwargs)
+        nt.__mul__ = product  # adding a method to the class definition
+        return nt
+
+    @wrapt.decorator
+    def type_decorator(wrapped, instance, args, kwargs):
+        nt = wrapped(*args, **kwargs)
+        nt.__mul__ = product
+        return nt
+
+    if isinstance(pydef, type):
+        return type_decorator(pydef)
+    else:
+        return factory_decorator(pydef)
 
 
-
-
-# @algebraic
+@algebraic
 class Empty(NamedTuple):
+    """
+    The Initial Element of the PreOrder on number of fields.
+    """
     pass
 
 
@@ -33,7 +56,7 @@ def test_record_empty():
 def test_record_create(field_types: typing.Dict[str, Type]):
 
     # dynamically build the record type
-    MyRecord = NamedTuple("MyRecord", fields=[ft for ft in field_types.items()])
+    MyRecord = algebraic(NamedTuple("MyRecord", fields=[ft for ft in field_types.items()]))
 
     assert MyRecord._fields == tuple(k for k in field_types.keys())
     assert MyRecord._field_types == OrderedDict({f:t for f, t in field_types.items()})
