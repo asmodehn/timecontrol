ad19f73bb6d9b93e347091094cca92972c954a82
AlexV <asmodehn@gmail.com>
2020-04-19 16:53:04 +0200

    committing almost working framedtuple before trying to solve data overflows in numpy...

--- timecontrol/framedtuple.py
+++ timecontrol/framedtuple.py
@@ -9,6 +9,99 @@ import inspect
 import hypothesis.strategies as st
 import hypothesis.extra.numpy as npst
 
+
+class EmptyFrameTupleMeta(type):
+    """ Specific Empty (meta)type for FrameTuple, for code clarity.
+    This is the Empty Type from ITT, but at the meta level.
+    """
+
+    def __new__(mcs, typename, bases, ns):
+        return super(EmptyFrameTupleMeta, mcs).__new__(mcs, "EmptyFrameTuple", (), {})
+
+    def __call__(cls, *args, **kwargs):
+        raise RuntimeError("EmptyFrameTuple cannot be instantiated")
+
+EmptyFrameTuple = EmptyFrameTupleMeta("EmptyFrameTuple", (), {})
+
+
+class UnitFramedTupleMeta(type):
+    """ Specific Unit (meta)type for FrameTuple, for code clarity.
+    This is implemented as an empty tuple.
+    There can potentially be multiple UnitTypes if the user make it so (via different typenames).
+    There can be only one unit meta type however (keeping the ITT setup).
+    """
+    _unit: typing.ClassVar
+
+    def __new__(mcs, typename, bases, ns):
+        # if the same type already exist, return it !
+        existing = UnitFrameTuples.get((typename,) + bases)
+        if existing:
+            return existing
+        else:
+            cls = super(UnitFramedTupleMeta, mcs).__new__(mcs, typename, bases, ns)
+            cls._unit = cls()
+            UnitFrameTuples[(typename,) + bases] = cls
+            return cls
+
+    def __init__(cls, typename, bases, ns):
+        super(UnitFramedTupleMeta, cls).__init__(typename, bases, ns)
+
+        cls._typename = typename
+
+    def __call__(cls, *args, **kwargs):
+        return cls._unit  # The only instance of the unit type
+
+    def __repr__(cls):
+        return f"<UnitFramedTuple <{cls._typename}> []>"
+
+    def __eq__(cls, other: FramedTupleMeta):
+        return (cls._typename == other._typename)
+
+    # we need to keep the usual hash behavior, even if we redefine equality, to keep usual python tools behavior (debugger!)
+    def __hash__(cls):  # TODO :  this need deep investigation...
+        return type.__hash__(cls)
+
+    def __iter__(cls):
+        return cls._unit
+
+    def __len__(cls):
+        return 1
+
+
+# TODO : avoid duplication, maybe there is already a location in the process global context ?
+UnitFrameTuples = dict()
+
+
+class EitherFrameTupleMeta(type):
+    """ Specific Either (meta)type for FramedTuple, for code clarity."""
+
+    _left: typing.ClassVar
+    _right: typing.ClassVar
+
+    def __new__(mcs, typename, bases, ns):
+        cls = super(EitherFrameTupleMeta, mcs).__new__(mcs, typename, bases, ns)
+        cls._unit = cls()
+
+        return cls
+
+    def __init__(cls, typename, bases, ns):
+        super(FramedTupleMeta, cls).__init__(typename, bases, ns)
+
+        # setting up typename
+        cls._typename = typename
+
+        # also setting up strategy
+        cls._strategy = st.just(cls._unit)
+
+    def __call__(cls, *args, **kwargs):
+        raise NotImplementedError
+
+
+
+
+
+
+
 class FramedTupleMeta(type):
 
     @classmethod
@@ -17,14 +110,25 @@ class FramedTupleMeta(type):
         # creating empty typeframe on the instance and adding it to the class (ie the type)
         # ns['typeframe'] = sf.Frame.from_records(records=[], columns=_ntbase._fields, dtypes=_ntbase._field_types)
 
-        # return {
-        #     '_typemap': {  # a map to convert a numpy dtype into a python type...
-        #
-        #     },
-        # }
-        return {}
+        return {
+            '_typemap': {  # a map to generate a python type...
+                ''
+
+            },
+        }
 
     def __new__(mcs, typename, bases, ns):
+
+        # dropping invalid annotations (None) to not have them created in the namedtuple and in the frame
+        _nonefields = [a for a, t in ns.get("__annotations__", {}).items() if t is None or t is type(None)]
+
+        if _nonefields:  # if at least one field is NoneType => we cannot create it => EmptyType
+            return EmptyFrameTuple
+            # This means we represent The EmptyType by NoneType
+            # => a function returning None is a function that *never* returns
+
+        # stripping NoneType fields for overall hygiene and sanity around here...
+        ns['__annotations__'] = {a: t for a, t in ns.get("__annotations__", {}).items() if a not in _nonefields}
 
         # calling NamedTupleMeta to also process defaults values for this record type
         ntbase = NamedTupleMeta(typename=f"{typename}NamedTuple", bases=bases, ns={**ns, **{
@@ -33,16 +137,37 @@ class FramedTupleMeta(type):
                 (type(getattr(s, f)), getattr(s, f)) == (type(getattr(s, f)), getattr(o, f)) for f in s._fields),
         }})
 
-        # we create a typeframe from the ntbase structure
-        ns['frame'] = sf.Frame.from_records(records=[], columns=ntbase._fields, dtypes=ntbase._field_types)
+        if not ntbase._fields:  # No fields ! => Unit type => no elements => no Frame
+            # we need to generate columns, types and strategies already while we have the tuple with proper python types
+            ns['_columns'] = ()
+            ns['_column_types'] = dict()
+            ns['_column_strategies'] = []
+            # TODO : defaults ?
 
-        # we create a class that inherits from namedtuple, but with our metaclass as a type.
-        return super(FramedTupleMeta, mcs).__new__(mcs, f"{typename}", bases + (ntbase,), ns)
+            return UnitFramedTupleMeta(typename=typename, bases=bases + (ntbase,), ns=ns)
+
+        else:
+            # we need to generate columns, types and strategies already while we have the tuple with proper python types
+            ns['_columns'] = ntbase._fields
+            ns['_column_types'] = ntbase._field_types
+            ns['_column_strategies'] = [(ft[0], st.from_type(ft[1])) for ft in ns['_column_types'].items()]
+            # TODO : defaults ?
+
+            # DO NOT TRUST dtypes here (empty frame doesnt record them)
+            # we create a typeframe from the ntbase structure
+            ns['frame'] = sf.Frame.from_records(records=[], columns=ntbase._fields)
+
+            # we create a class that inherits from namedtuple, but with our metaclass as a type.
+            return super(FramedTupleMeta, mcs).__new__(mcs, f"{typename}", bases + (ntbase,), ns)
 
     def __init__(cls, typename, bases, ns):
         super(FramedTupleMeta, cls).__init__(typename, bases, ns)
 
+        # setting up typename
         cls._typename = typename
+
+        # also setting up strategy
+        cls._strategy = st.builds(cls, **{att: s for att, s in cls._column_strategies})
 
     def __call__(cls, *args, **kwargs):
         # to wrap the behavior of the static __new__ in the class (and not the __new__ of the metaclass)
@@ -50,34 +175,22 @@ class FramedTupleMeta(type):
 
         # store into the frame
         records = [e for e in cls.frame.iter_tuple(1)]
-        cls.frame = sf.Frame.from_records(records=records + [inst])
-
-
-
+        try:
+            cls.frame = sf.Frame.from_records(records=records + [inst], columns=cls._columns, dtypes=cls._column_types)
+        except ValueError as ve:
+            print(f"ValueError : {inst} cannot be stored in the frame")
+            raise ve
+        except OverflowError as oe:
+            print(f"OverflowError : {inst} cannot be strored in the frame")  # TODO :WORKINPROGRESS dependent typing for physical data bounds...
+            raise oe
         return inst
-
-    @property
-    def _columns(cls):  # this is the structure of the type, but dynamic for the metaclass !
-        return list(cls.frame.columns)
-
-    @property
-    def _column_types(cls):  # this is the structure of the type, but dynamic for the metaclass !
-        return [dt for dt in cls.frame.dtypes.items()]
-
-    @property
-    def _column_strategies(cls):  # this is the structure of the type, but dynamic for the metaclass !
-        return [(dt[0], npst.from_dtype(dt[1])) for dt in cls.frame.dtypes.items()]
-
-    @property
-    def _strategy(cls):
-        return st.builds(cls, **{att: s for att, s in cls._column_strategies})
 
     def __repr__(cls):
         # dtypes_dict = {k: v for k, v in self.frame.dtypes.items()}
         return f"<FramedTuple <{cls._typename}> {cls._column_types}>"
 
     def __eq__(cls, other: FramedTupleMeta):
-        return (cls._typename == other._typename) and (cls._column_types == other._column_types)
+        return other is not EmptyFrameTuple and (cls._typename == other._typename) and (cls._column_types == other._column_types)
 
     # we need to keep the usual hash behavior, even if we redefine equality, to keep usual python tools behavior (debugger!)
     def __hash__(cls):  # TODO :  this need deep investigation...
--- timecontrol/tests/test_framedtuple.py
+++ timecontrol/tests/test_framedtuple.py
@@ -8,7 +8,7 @@ from hypothesis import given, infer, Verbosity, settings
 import hypothesis.strategies as st
 
 from timecontrol.tests.strats.st_framedtuple import st_FramedTuple, identifier_strategy, typehints_strategy
-from timecontrol.framedtuple import FramedTupleMeta
+from timecontrol.framedtuple import FramedTupleMeta, EmptyFrameTuple, UnitFramedTupleMeta
 
 
 class TestFramedTuple(unittest.TestCase):
@@ -16,33 +16,84 @@ class TestFramedTuple(unittest.TestCase):
     @settings(verbosity=Verbosity.verbose)
     @given(data=st.data(), ft=st_FramedTuple())
     def test_framedtuple_instance_equality(self, data, ft):
-        ta = data.draw(ft._strategy)
 
-        tb = data.draw(ft._strategy)
+        if ft is not EmptyFrameTuple:  # TODO : strategy param to avoid generating this ?
 
-        assert (ta == tb) == (ta._field_types == tb._field_types and all(tuple(getattr(ta, f) == getattr(tb, f) for f in ta._fields)))
+            ta = data.draw(ft._strategy)
+
+            tb = data.draw(ft._strategy)
+
+            assert (ta == tb) == (ta._field_types == tb._field_types and all(tuple(getattr(ta, f) == getattr(tb, f) for f in ta._fields)))
 
     @settings(verbosity=Verbosity.verbose)
     @given(data=st.data(), fta=st_FramedTuple(), ftb=st_FramedTuple())
     def test_framedtuple_equality(self, data, fta, ftb):
 
-        assert (fta == ftb) == (fta.__name__ == ftb.__name__ and fta._column_types == ftb._column_types)
+        assert (fta == ftb) == (
+                fta == ftb == EmptyFrameTuple or (
+                    fta.__name__ == ftb.__name__ and fta._column_types == ftb._column_types
+        ))
 
-        # extensional equality is not intensional
-        assert not fta is ftb
+        # extensional equality is not intensional (careful with empty type)
+        assert (fta == ftb == EmptyFrameTuple) or (not fta is ftb)
 
         # copy is just by reference -> intensionally equal
         ftc = fta
         assert ftc is fta
 
-        # instance equality implies type equality ! (more strict than usual python)
-        ta = data.draw(fta._strategy)
-        tb = data.draw(ftb._strategy)
-        if ta == tb:
-            assert fta == ftb
+        if fta is not EmptyFrameTuple and ftb is not EmptyFrameTuple:
+            # instance equality implies type equality ! (more strict than usual python)
+            ta = data.draw(fta._strategy)
+            tb = data.draw(ftb._strategy)
+            if ta == tb:
+                assert fta == ftb
+
+    @settings(verbosity=Verbosity.verbose)
+    @given(data=st.data(), ft=st_FramedTuple())
+    def test_framedtuple_instance_iterator(self, data, ft):
+
+        if ft is not EmptyFrameTuple:
+            ta = data.draw(ft._strategy)
+
+            # tuples are iterable as usual
+            for n, v in ta._asdict().items():
+                assert n in ta._fields
+                assert type(v) is ta._field_types[n]
+
+    @settings(verbosity=Verbosity.verbose)
+    @given(data=st.data(), fta=st_FramedTuple())
+    def test_framedtuple_iterator(self, data, fta):
+
+        if fta is not EmptyFrameTuple:  # TODO : strategy param to avoid generating this ?
+
+            if not isinstance(fta, UnitFramedTupleMeta):  # Unit Types already have their value instantiated...
+                with self.assertRaises(StopIteration) as si:
+                    next(iter(fta))
+
+            ta = data.draw(fta._strategy)
+
+            # type iterate on created tuple instances
+            for t in fta:
+                assert type(t) is type(ta) is fta  # the framedtuple is the type
+                assert isinstance(t, fta) and isinstance(t, tuple)
 
 
-
+    # @settings(verbosity=Verbosity.verbose)
+    # @given(data=st.data(), ft=st_FramedTuple())
+    # def test_framedtuple_instance_indexer(self, data, ft):
+    #     ta = data.draw(ft._strategy)
+    #
+    #     tb = data.draw(ft._strategy)
+    #
+    #     # fields are the index and point to a value in the specified type
+    #     assert all(type(ta[f]) is type(tb[f]) is t for f, t in ta._column_types)
+    #
+    # @settings(verbosity=Verbosity.verbose)
+    # @given(data=st.data(), fta=st_FramedTuple(), ftb=st_FramedTuple())
+    # def test_framedtuple_indexer(self, data, fta, ftb):
+    #
+    #     # columns are the index and point to the type of that column
+    #     assert all(fta[c] is ftb[c] is t for c, t in fta._column_types)
 
 
 if __name__ == '__main__':
