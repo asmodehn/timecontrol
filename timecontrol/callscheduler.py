cdef7760639c9cbb05d6c5162aa6217002c6cd16
AlexV <asmodehn@gmail.com>
2020-08-01 15:23:49 +0200

    attempting a simple callscheduler, leveraging an async generator...

---
+++ timecontrol/callscheduler.py
@@ -0,0 +1,112 @@
+import asyncio
+import inspect
+import time
+import typing
+from datetime import MINYEAR, datetime, timedelta
+
+import wrapt
+
+from timecontrol.eventful import TimePeriod, TimePoint
+
+
+def callscheduler(# TODO pass log:  = None,  Maybe pass a function / async callable instead ?
+        #: https://en.wikipedia.org/wiki/Rate_limiting
+        # But this is expressed in time units (minimal guaranteed "no-call" period)
+        ratelimit: typing.Optional[TimePeriod] = None,
+
+        #: https://en.wikipedia.org/wiki/Temporal_resolution
+        #timeframe: typing.Optional[TimePeriod] = None,
+
+        # Not useful here, only for loopaccelerator
+        timer: typing.Callable[[], TimePoint] = datetime.now,
+        sleeper: typing.Callable[[TimePeriod], None]=None):
+
+    _last = timer()
+    # Setting last as now, to prevent accidental bursts on creation.
+
+    _inner_last = datetime(year=MINYEAR, month=1, day=1)
+    # Setting last as long time ago, to force speedup on creation.
+
+    def decorator(wrapper):
+        nonlocal sleeper
+
+        @wrapt.decorator
+        def callscheduled_function(wrapped, instance, args, kwargs):
+
+            nonlocal _last
+
+            while ratelimit:  # TODO : find a way to stop/cancel this ?
+
+                # Measure time
+                now = timer()
+
+                # print(f"{now} - {_last} = {now - _last}")
+                # sleep if needed (this can be addressed locally)
+                if now - _last > ratelimit:
+                    # Call too slow. calling now
+                    yield wrapped(*args, **kwargs)
+
+                    # need to sleep to wait next calltime
+                    sleeptime = ratelimit - ((now - _last) - ratelimit)  # sleep time should be less than ratelimit
+                    if isinstance(sleeptime, timedelta):
+                        sleeptime=sleeptime.total_seconds()
+                    print(f"sleeps for {sleeptime}")
+                    # sleeps expected time period - already elapsed time
+                    sleeper(sleeptime)
+
+                    _last = timer()
+
+                # mini sleep to not lock the loop
+                sleeper(0.1)
+
+            # return None mandatory for generators
+
+        @wrapt.decorator
+        async def async_calllimited_function(wrapped, instance, args, kwargs):
+
+            nonlocal _last
+
+
+        # Note : in this decorator generators or classes are not considered...
+        # it throttles only the usual call.
+
+        # checking for async first, to avoid too much if-nesting
+        if inspect.iscoroutinefunction(wrapper):
+            if sleeper is None:
+                sleeper = asyncio.sleep
+            wrap = async_calllimited_function(wrapper)
+
+            # then the more general case
+        elif inspect.isfunction(wrapper) or inspect.ismethod(wrapper):
+            if sleeper is None:
+                sleeper = time.sleep
+            wrap = callscheduled_function(wrapper)
+
+            # did we forget any usecase ?
+        else:
+            raise NotImplementedError(f"eventful doesnt support decorating {wrapper}")
+
+        return wrap
+    return decorator
+
+
+if __name__ == '__main__':
+
+    @callscheduler(ratelimit=timedelta(seconds=2))
+    def printer(*args, **kwargs):
+        print(f"{datetime.now()} : {args}, {kwargs}")
+
+    for call in printer("the", "answer", "is", 42, answer=42):
+        print(call)
+        pass  # no printing, it would duplicate the inner call
+
+    # @callscheduler(ratelimit=timedelta(seconds=2))
+    # async def async_printer(*args, **kwargs):
+    #     print(f"{datetime.now()} : {args}, {kwargs}")
+    #
+    # async def async_runner():
+    #
+    #     async for call in printer("the", "async", "answer", "is", 42, answer=42):
+    #         pass  # no printing, it would duplicate the inner call
+    #
+    # asyncio.run(async_runner())
