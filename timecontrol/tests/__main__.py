dfe44201316333d2ce490938ac864a7ca3f72ddb
AlexV <asmodehn@gmail.com>
2020-08-03 18:13:27 +0200

    enabling travis tests for 3.8 and fixing tests.__main__

--- timecontrol/tests/__main__.py
+++ timecontrol/tests/__main__.py
@@ -9,23 +9,19 @@ uses integrated python tools only.
 # import your test modules
 if __package__ is not None:
     from . import (
-        test_underlimiter,
-        test_overlimiter,
-        test_command,
-        test_underlimited_command,
+        test_calllimiter,
+        test_callscheduler
     )
 else:
-    import test_underlimiter, test_overlimiter, test_command, test_underlimited_command
+    import test_calllimiter, test_callscheduler
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(test_underlimiter))
-suite.addTests(loader.loadTestsFromModule(test_overlimiter))
-suite.addTests(loader.loadTestsFromModule(test_command))
-suite.addTests(loader.loadTestsFromModule(test_underlimited_command))
+suite.addTests(loader.loadTestsFromModule(test_calllimiter))
+suite.addTests(loader.loadTestsFromModule(test_callscheduler))
 
 # initialize a runner, pass it your suite and run it
 runner = unittest.TextTestRunner(verbosity=3)
